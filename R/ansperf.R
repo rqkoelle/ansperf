12bc93b1f593bbd4f8bb4c1a556f6192d4f1d0f8
unknown <rkoelle@ESSP0114.sky.corp.eurocontrol.int>
2015-09-03 16:03:38 +0200

    updated DESCRIPTION, LICENSE, and package help page

---
+++ R/ansperf.R
@@ -0,0 +1,13 @@
+#' ansperf: a package for the analysis of ANS Performance
+#'
+#' The ansperf package provides tools to support the analysis
+#' of the performance of air navigation services (ANS).
+#' The aim is to provide a common set of functions for the analysis
+#' and visualisation based on a standard set of performance data.
+#'
+#' @section ansperf functions:
+#' to be developed a set / summary of functions
+#'
+#' @docType package
+#' @name ansperf
+NULL
